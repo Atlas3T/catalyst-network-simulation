97b74c0f8d9e8b64fa0f759cd1facc2e4322a2be
fran <fran@fran.com>
2018-09-11 15:13:08 +0100

    updated files included

--- python/PeerDist/Propagation.py
+++ python/PeerDist/Propagation.py
@@ -1,5 +1,6 @@
 import numpy
 import scipy.io
+from numpy.core.multiarray import ndarray
 from scipy.special import factorial
 import os.path
 numpy.set_printoptions(threshold=numpy.inf)
@@ -76,7 +77,7 @@ def isValid(peers,N,p):
         if numpy.count_nonzero(b==i)!=p:
             print("invalid")
             return False
-    print(peers)
+    #print(peers)
     return True
 
 
@@ -90,18 +91,8 @@ def getPeerDistOrDieTrying(N,p):
 
 
 def sendToPeers(peers,messageDist,time):
-    print("messsage dist:")
-    #print(messageDist)
-    sentTo = numpy.multiply(messageDist,peers)
-    #print("sent to")
-    #print(sentTo)
-    x = numpy.nonzero(sentTo)
-    nor = x[0]
-    #print(nor)
-    for p in nor:
-        if messageDist[p] == 0:
-            messageDist[p] = time
-
+    p = peers[messageDist == time,:]
+    messageDist[p[:]]=numpy.where(messageDist[p[:]]==-1,time+1,messageDist[p[:]])
     return messageDist
 
 def getFilePathRoot():
@@ -111,6 +102,9 @@ def getFilePathRoot():
 
 def getPeerDistFilePath(N,p, s):
     return getFilePathRoot() + '/peer_dist_' + str(N) + '_' + str(p) + "_" + str(s)
+
+def getProbDistFilePath(N,p, x, i):
+    return getFilePathRoot() + '/prob_dist_' + str(N) + '_' + str(p) + "_" + str(x)+ "_" + str(i)
 
 def savePeerDist(N,p,x):
 
@@ -129,17 +123,59 @@ def loadPeerDist(N,p,n):
 
 
 def hasMessageSpreadToAllNodes(messageDist):
-    return not numpy.any(messageDist==0)
+    return not numpy.any(messageDist==-1)
 
 def disperseMessage(peers,s):
-    N = len(peers[:,1])
-    messageDist = numpy.zeros(N)
-    time = 1
+    N = len(peers[:,0])
+    messageDist = numpy.full(N,-1)
+    time = 0
     messageDist[s] = time
     while (hasMessageSpreadToAllNodes(messageDist)==False):
-        time+=1
         messageDist=sendToPeers(peers,messageDist,time)
+        time += 1
     return messageDist
+
+def saveDisperseMessageDist(N,p,x,iterations_startNode):
+    probDist = dict()
+    for ii in range(0,x):
+        peers = loadPeerDist(N,p,ii)
+        nodes = numpy.arange(N)
+        maxIndices = N-1
+
+        for i in range(0,iterations_startNode):
+            randStartNodeIndex = numpy.random.randint(0, N-1)
+            randNode = nodes[randStartNodeIndex]
+            nodes[maxIndices], nodes[randStartNodeIndex] = nodes[randStartNodeIndex], nodes[maxIndices]
+            messageDist = disperseMessage(peers,randNode)
+            unique_elements, counts_elements = numpy.unique(messageDist, return_counts=True)
+            for j in range(0,len(unique_elements)):
+                numOfHops = unique_elements[j]
+                if numOfHops in probDist:
+                    probDist[numOfHops] += counts_elements[j]
+                else:
+                    probDist[numOfHops] = counts_elements[j]
+
+    for k in probDist.keys():
+        probDist[k]/=(N*iterations_startNode*x)
+    X = len(probDist.keys())
+    probs: ndarray = numpy.zeros((2,X))
+    jj = 0
+    for k,v in probDist.items():
+        probs[0,jj]=k
+        probs[1,jj]=v
+        jj+=1
+    print(probs)
+    fileName = getProbDistFilePath(N, p, x, iterations_startNode)
+    scipy.io.savemat(fileName, {"probDist": probs}, appendmat=True)
+
+def loadDisperseMessageDist(N,p,x,iterations_startNode):
+
+    fileName = getProbDistFilePath(N, p, x, iterations_startNode)
+    contents = scipy.io.loadmat(fileName,  appendmat=True)
+    probDist = contents['probDist']
+    return probDist
+
+
 
 
 
--- python/PeerDist/main.py
+++ python/PeerDist/main.py
@@ -5,18 +5,32 @@ import Propagation
 
 def main():
     start = time.time()
-    N=100000;
+    N=1000000;
     p=10;
-    x=2;
+    x=10;
     Propagation.savePeerDist(N,p,x)
-    #peers = Propagation.loadPeerDist(N,p,2)
-    duration = time.time() - start
-    print(duration)
-    #m = Propagation.disperseMessage(peers,3)
 
-    #plt.hist(m, bins = [0,1,2,3,4,5,6,7,8,9])
-    #plt.show()
 
+        Propagation.saveDisperseMessageDist(100,p,x,100)
+        Propagation.saveDisperseMessageDist(1000, p, x, 100)
+        Propagation.saveDisperseMessageDist(10000, p, x, 100)
+        Propagation.saveDisperseMessageDist(100000, p, x, 100)
+
+    Propagation.saveDisperseMessageDist(1000000, p, 2, 100)
+
+    a = Propagation.loadDisperseMessageDist(100,p,x, 100)
+    b = Propagation.loadDisperseMessageDist(1000, p, x, 100)
+    c = Propagation.loadDisperseMessageDist(10000, p, x, 100)
+    d = Propagation.loadDisperseMessageDist(100000, p, x, 100)
+    e = Propagation.loadDisperseMessageDist(1000000, p, 2, 100)
+    plt.plot(a[0, :], a[1, :])
+    plt.plot(b[0, :], b[1, :])
+    plt.plot(c[0, :], c[1, :])
+    plt.plot(d[0, :], d[1, :])
+    plt.plot(e[0, :], e[1, :])
+    plt.show()
+    #duration = time.time() - start
+    #print(duration)
 
 if __name__ == '__main__':
     main()
