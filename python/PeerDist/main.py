dc62c5b6d3aae90abea292b4cd408c08d0d6a9bc
FranSSL <fran.sl@atlascity.io>
2018-09-19 14:18:45 +0100

    peer dist now cythonised

--- python/PeerDist/main.py
+++ python/PeerDist/main.py
@@ -1,27 +1,28 @@
 import time
 import matplotlib.pyplot as plt
-import Propagation
-
+import propagation
+import profile
 
 def main():
+
     start = time.time()
     N=1000000;
     p=10;
-    x=10;
-    Propagation.savePeerDist(N,p,x)
-
+    x=20;
+    #Propagation.savePeerDist(N,p,x)
+    propagation.generatePeerDist(N,p,x)
     if False:
-        Propagation.saveDisperseMessageDist(100,p,x,100)
-        Propagation.saveDisperseMessageDist(1000, p, x, 100)
-        Propagation.saveDisperseMessageDist(10000, p, x, 100)
-        Propagation.saveDisperseMessageDist(100000, p, x, 100)
+        propagation.saveDisperseMessageDist(100,p,x,100)
+        propagation.saveDisperseMessageDist(1000, p, x, 100)
+        propagation.saveDisperseMessageDist(10000, p, x, 100)
+        propagation.saveDisperseMessageDist(100000, p, x, 100)
 
-        Propagation.saveDisperseMessageDist(1000000, p, 2, 100)
+        propagation.saveDisperseMessageDist(1000000, p, 2, 100)
 
-        a = Propagation.loadDisperseMessageDist(100,p,x, 100)
-        b = Propagation.loadDisperseMessageDist(1000, p, x, 100)
-        c = Propagation.loadDisperseMessageDist(10000, p, x, 100)
-        d = Propagation.loadDisperseMessageDist(100000, p, x, 100)
+        a = propagation.loadDisperseMessageDist(100,p,x, 100)
+        b = propagation.loadDisperseMessageDist(1000, p, x, 100)
+        c = propagation.loadDisperseMessageDist(10000, p, x, 100)
+        d = propagation.loadDisperseMessageDist(100000, p, x, 100)
     #e = Propagation.loadDisperseMessageDist(1000000, p, 2, 100)
         plt.plot(a[0, :], a[1, :])
         plt.plot(b[0, :], b[1, :])
@@ -33,4 +34,5 @@ def main():
     print(duration)
 
 if __name__ == '__main__':
+    #profile.run('main()')
     main()
