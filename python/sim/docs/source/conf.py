779e3a8f3ff4a9d031c3153be5180034a6eac89d
fran <simulation@server>
2018-10-23 18:15:20 +0100

    ugh

--- python/sim/docs/source/conf.py
+++ python/sim/docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [
     'sphinx.ext.imgmath',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    #'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
