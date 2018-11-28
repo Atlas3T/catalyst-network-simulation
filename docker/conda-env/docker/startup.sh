3ec14bba86547c73381dc910e43ad200c7dd5062
fran <fran@Atlass-MacBook-Pro.local>
2018-11-28 19:55:39 +0000

    gitignore

--- docker/conda-env/docker/startup.sh
+++ docker/conda-env/docker/startup.sh
@@ -1,2 +1,5 @@
 echo "conda activate $(head -1 envs/environment.yml | cut -d' ' -f2)" >> ~/.bashrc
+#make -C lib
+jupyter nbextensions_configurator enable
 jupyter notebook --no-browser --ip=0.0.0.0
+
